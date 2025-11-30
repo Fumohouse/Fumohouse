9d23f8478caba32ac26b2cfb26dfe118581a8262
stickerfield <247050462+stickerfield@users.noreply.github.com>
2025-11-30 17:40:06 -0400

    add basic fumo select UI
    Some quirks (mostly UI, code quirks are commented):
    
    - The UI is too small, although I think the location I placed it in
    works fine it would be better for it to function as a separate screen
    (a small favorites panel is what I thought to put instead, I believe it
    would work better on a fullscreen menu though)
    
    - The FumoSelect Control as is placed on the game_menu scene would work
    much better as a separate scene (the rest of the UI is designed like
    this)
    
    - The UI has no animations

---
+++ addons/@fumohouse/fumo_select/select_grid.gd
@@ -0,0 +1,42 @@
+extends GridContainer
+
+# TODO: does not work well (i.e at all) with the modules design
+# closest I can think to the current architecture is to add autoloads?
+# to allow organizing fumo files however the author wishes instead of hardcoding a path
+# that does require designing loading the other parts in the database (not handled at all)
+const _PRESET_DIR = "res://addons/@fumohouse/fumo_models/resources/presets"
+
+@onready var _item_template: Button = %FumoGridItem
+
+
+func _ready():
+	#_item_template.reparent(null)
+
+	# TODO: not a good idea to get_node like this
+	var char_manager := get_node("/root/Playground/CharacterManager") as CharacterManagerBase
+	if not char_manager:
+		push_error("No character manager found.")
+		return
+
+	var preset_dir := DirAccess.open(_PRESET_DIR)
+	if not preset_dir:
+		push_error("Failed to open presets directory.")
+		return
+
+	preset_dir.list_dir_begin()
+	while true:
+		var preset_filepath := preset_dir.get_next()
+		if preset_filepath == "":
+			break
+		# not sure if gdscript has a Path type or a join() utility function
+		var preset := load(preset_dir.get_current_dir() + "/" + preset_filepath) as Appearance
+		if not preset:
+			push_warning("Unrecognized preset: " + preset_filepath)
+			continue
+
+		var item := _item_template.duplicate()
+		item.text = preset.display_name
+		# TODO: change fumos without going back to a spawnpoint
+		item.pressed.connect(func(): char_manager._spawn_character(preset, null))
+
+		self.add_child(item)
