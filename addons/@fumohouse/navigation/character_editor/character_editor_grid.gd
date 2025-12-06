e140e5a259a5b2d4bf1946c39c4250413146bcf2
stickerfield <247050462+stickerfield@users.noreply.github.com>
2025-12-05 20:59:23 -0400

    navigation: flesh out character editor screen UX
    * Settles on an experimental UI with tabs for presets and customization (to showcase functionality mostly)
    * Switches viewport to use Fumo, to remove world dependency when moving into fumo mdule
    * Adds a search filter for presets

--- addons/@fumohouse/navigation/character_editor/character_editor_grid.gd
+++ addons/@fumohouse/navigation/character_editor/character_editor_grid.gd
@@ -6,11 +6,14 @@ extends VBoxContainer
 
 @onready var _active_label: Label = %ActiveLabel
 
+@onready var _search_edit: LineEdit = %SearchEdit
+
 
 func _ready():
 	_update_label(fumo_appearances._staging)
 	fumo_appearances.staging_changed.connect(_update_label)
 	%ApplyButton.pressed.connect(fumo_appearances.apply)
+	_search_edit.text_changed.connect(_filter_presets)
 
 	_update_presets()
 	fumo_appearances.entries_updated.connect(_update_presets)
@@ -25,6 +28,15 @@ func _update_presets():
 		var item := CharacterEditorGridItem.new(preset)
 		item.pressed.connect(_stage_appearance.bind(preset))
 		_grid.add_child(item)
+
+
+func _filter_presets(query: String):
+	for child in _grid.get_children():
+		var preset := child as CharacterEditorGridItem
+		if preset == null:
+			push_warning("Unknown grid item: '%s'" % child.name)
+			continue
+		preset.visible = query.is_empty() or preset.text.containsn(query)
 
 
 func _stage_appearance(appearance: Appearance):
--- addons/@fumohouse/navigation/character_editor/character_viewport.gd
+++ addons/@fumohouse/navigation/character_editor/character_viewport.gd
@@ -1,11 +1,22 @@
 extends SubViewport
 
 
-@onready var _character_manager: CharacterManager = %CharacterManager
+@onready var _character: Fumo = %Fumo
+
+@onready var _camera_controller: CameraController = %CameraController
 
 @onready var fumo_appearances: FumoAppearances = FumoAppearances.get_singleton()
 
 
 func _ready() -> void:
-	_character_manager.debug_character = DebugCharacter.new()
-	_character_manager._spawn_character(fumo_appearances._staging)
+	load_appearance()
+	fumo_appearances.staging_changed.connect(load_appearance)
+
+	_camera_controller.camera_rotation.y = PI
+	_character.camera = _camera_controller
+	#_character.state.ctx.motion = _character._get_motion()
+
+
+func load_appearance(appearance: Appearance = fumo_appearances._staging):
+	_character.appearance_manager.appearance = appearance
+	_character.appearance_manager.load_appearance()
