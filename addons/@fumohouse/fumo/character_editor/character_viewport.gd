6328be367b2779fd87a32893a06efeff0b1b32f5
stickerfield <247050462+stickerfield@users.noreply.github.com>
2025-12-06 15:32:45 -0400

    fumo: improve char viewport visuals and split active/staging

--- addons/@fumohouse/fumo/character_editor/character_viewport.gd
+++ addons/@fumohouse/fumo/character_editor/character_viewport.gd
@@ -7,16 +7,31 @@ extends SubViewport
 
 @onready var fumo_appearances: FumoAppearances = FumoAppearances.get_singleton()
 
+## Underlying Appearance to display
+@export_enum(
+	&"Active",
+	&"Staging"
+)
+var appearance_provider: String
+
 
 func _ready() -> void:
-	load_appearance()
-	fumo_appearances.staging_changed.connect(load_appearance)
+	match appearance_provider:
+		&"Active":
+			load_appearance(fumo_appearances._active)
+			fumo_appearances.active_changed.connect(load_appearance)
+		&"Staging":
+			load_appearance(fumo_appearances.staging)
+			fumo_appearances.staging_changed.connect(load_appearance)
+		_:
+			push_error("No FumoAppearances provider selected!")
+			return
 
 	_camera_controller.camera_rotation.y = PI
 	_character.camera = _camera_controller
 	#_character.state.ctx.motion = _character._get_motion()
 
 
-func load_appearance(appearance: Appearance = fumo_appearances.staging):
+func load_appearance(appearance: Appearance):
 	_character.appearance_manager.appearance = appearance
 	_character.appearance_manager.load_appearance()
