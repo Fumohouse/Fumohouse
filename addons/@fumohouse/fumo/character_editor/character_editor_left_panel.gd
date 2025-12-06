5d4292ca7b559dae60639c114c9e0025c8132126
stickerfield <247050462+stickerfield@users.noreply.github.com>
2025-12-06 02:53:10 -0400

    fumo: implement character editor scale slider

--- addons/@fumohouse/fumo/character_editor/character_editor_left_panel.gd
+++ addons/@fumohouse/fumo/character_editor/character_editor_left_panel.gd
@@ -6,12 +6,26 @@ extends VBoxContainer
 
 @onready var _apply_button: Button = %ApplyButton
 
+@onready var _scale_slider: HSlider = %ScaleSlider
+
+@onready var _scale_label: Label = %ScaleLabel
+
 
 func _ready():
 	_update_label(fumo_appearances.staging)
 	fumo_appearances.staging_changed.connect(_update_label)
+
 	_apply_button.pressed.connect(fumo_appearances.apply)
+
+	_scale_slider.value_changed.connect(_update_scale)
 
 
 func _update_label(appearance: Appearance):
 	_active_label.text = appearance.display_name
+	_scale_label.text = "%.f%%" % (appearance.config.get(&"scale") * 100)
+
+
+func _update_scale(scale: float):
+	fumo_appearances.with_staging(func(staging: Appearance):
+		staging.config.set(&"scale", scale)
+	)
