7a1bedbb60db3e72dc727c0928d0f4423db587bd
stickerfield <247050462+stickerfield@users.noreply.github.com>
2025-12-06 02:07:30 -0400

    split left panel and presets code on character editor
    Fixes crash on scene load

--- addons/@fumohouse/fumo/character_editor/character_editor_presets.gd
+++ addons/@fumohouse/fumo/character_editor/character_editor_presets.gd
@@ -4,23 +4,12 @@ extends VBoxContainer
 
 @onready var _grid: GridContainer = %Grid
 
-@onready var _active_label: Label = %ActiveLabel
-
 @onready var _search_edit: LineEdit = %SearchEdit
 
 
 func _ready():
-	_update_label(fumo_appearances._staging)
-	fumo_appearances.staging_changed.connect(_update_label)
-	%ApplyButton.pressed.connect(fumo_appearances.apply)
-	_search_edit.text_changed.connect(_filter_presets)
-
 	_update_presets()
 	fumo_appearances.entries_updated.connect(_update_presets)
-
-
-func _update_label(appearance: Appearance):
-	_active_label.text = appearance.display_name
 
 
 func _update_presets():
