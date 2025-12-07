eaa411355a70a8137737484bc4d0d6fa20514c6e
stickerfield <247050462+stickerfield@users.noreply.github.com>
2025-12-07 00:49:20 -0400

    fumo: add character customization logic

--- addons/@fumohouse/fumo/character_editor/character_editor_custom_section.gd
+++ addons/@fumohouse/fumo/character_editor/character_editor_custom_section.gd
@@ -1,6 +1,8 @@
 class_name CharacterEditorCustomSection
 extends MarginContainer
 
+@onready var fumo_appearances: FumoAppearances = FumoAppearances.get_singleton()
+
 @onready var _grid: GridContainer = %Grid
 
 @onready var _title: Label = %Title
@@ -15,4 +17,13 @@ func _ready():
 func add_item(part: PartData):
 	var button := Button.new()
 	button.text = part.display_name
+	button.pressed.connect(_set_part.bind(part))
 	_grid.add_child(button)
+
+
+func _set_part(part: PartData):
+	fumo_appearances.with_staging(
+		func(staging: Appearance):
+			if not staging.attached_parts.erase(part.id):
+				staging.attached_parts[part.id] = part
+	)
