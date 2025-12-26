5a3c6d162013fc3e594e6055ecf963350f3f9c22
stickerfield <247050462+stickerfield@users.noreply.github.com>
2025-12-26 14:08:42 -0400

    fumo: change CharacterViewport impl to TextureRect
    Lacking AutoScaleViewport seems to cause issues on HiDPI displays (can't
    test without one),
    
    SubViewportContainer changes the UI size if the internal resolution of
    the viewport is changed, switch to TextureRect as seen in 2022

--- addons/@fumohouse/fumo/character_editor/character_viewport.gd
+++ addons/@fumohouse/fumo/character_editor/character_viewport.gd
@@ -1,4 +1,4 @@
-extends SubViewportContainer
+extends TextureRect
 
 @export var disable_input: bool = false
 
@@ -9,9 +9,33 @@ extends SubViewportContainer
 
 
 func _ready() -> void:
+	texture = _subviewport.get_texture()
 	_subviewport.gui_disable_input = disable_input
+
+	_scale_viewport()
+	get_window().size_changed.connect(_scale_viewport)
+	resized.connect(_scale_viewport)
 
 	_camera_controller.camera_rotation.y = PI
 
 	character.appearance_manager.base_camera_offset = 2.0
 	character.camera = _camera_controller
+
+
+func _scale_viewport():
+	var window := get_window()
+	var scale: Vector2i = window.size / window.content_scale_size
+	_subviewport.size = size * max(scale.x, scale.y)
+
+
+func _gui_input(event: InputEvent) -> void:
+	var window := get_window()
+	var scale: Vector2 = window.size / window.content_scale_size
+
+	if event is InputEventMouse:
+		event.position *= scale
+	if event is InputEventMouseMotion:
+		event.relative *= scale
+
+	_subviewport.push_input(event, true)
+	accept_event()
