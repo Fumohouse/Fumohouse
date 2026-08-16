961492dba351f7a4eab625dc3982d67cbb21093d
voidedWarranties <kyo@seki.pw>
2026-08-16 14:14:20 -0700

    Recolor shader, make more parts recolorable

--- addons/@fumohouse/character_appearance/customization/color_customizer.gd
+++ addons/@fumohouse/character_appearance/customization/color_customizer.gd
@@ -2,22 +2,33 @@ class_name SingleColorCustomizer
 extends PartCustomizer
 ## A part customizer that changes the color of one material.
 
-## The mesh to apply the color to.
-@export var mesh: MeshInstance3D
+## The meshes to apply the color to.
+@export var meshes: Array[MeshInstance3D]
+
+## A one-to-one mapping with [member meshes] indicating which config parameter
+## to use for the color. If this array is shorter than [member meshes], the last
+## entry is used for the remaining meshes.
+@export var attributes: PackedStringArray = ["color"]
 
 ## The material index to customize.
 @export var material_index := 0
 
 
 func _update(config: Dictionary):
-	assert(mesh != null, "A mesh is required to customize the color.")
-	assert(config.has(&"color"), "color is a required key in the part configuration.")
+	for i in meshes.size():
+		var mesh := meshes[i]
+		var attribute := StringName(
+			attributes[i] if i < attributes.size() else attributes[attributes.size() - 1]
+		)
+		var color: Color = config.get(attribute, Color.WHITE)
+		var material := mesh.get_active_material(material_index)
 
-	var color: Color = config.get(&"color") if config.has(&"color") else Color.WHITE
-	var material := mesh.get_active_material(material_index)
-
-	if material is StandardMaterial3D:
-		material.albedo_color = color
-	elif material is ShaderMaterial:
-		if material.get_shader_parameter("albedo") != null:
-			material.set_shader_parameter("albedo", color)
+		if material is StandardMaterial3D:
+			material.albedo_color = color
+		elif material is ShaderMaterial:
+			if material.get_shader_parameter("recolor") != null:
+				material.set_shader_parameter("recolor", color)
+				if material.get_shader_parameter("albedo") != null:
+					material.set_shader_parameter("albedo", Color.WHITE)
+			elif material.get_shader_parameter("albedo") != null:
+				material.set_shader_parameter("albedo", color)
